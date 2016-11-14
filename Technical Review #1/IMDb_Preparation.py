c91157a87a48657702c6f8d12f718735db2ff99d
Jason Lee <jlee66@babson.edu>
2016-11-13 20:35:21 -0500

    Technical Review #1

---
+++ Technical Review #1/IMDb_Preparation.py
@@ -0,0 +1,13 @@
+#Predict the IMDb scores
+
+##Team Ignus: Wayne Kwon, Jason Lee, Alyssa Wu, and Sandy |
+
+###Background and context
+Dataset: [This link](https://www.kaggle.com/deepmatrix/imdb-5000-movie-dataset)
+
+We have found an existing data set containing over 5000 data points for movies linked above. The data set contains 28 variables ranging from information such as the director name, main actors and the number of likes that each actor has on Facebook, the movie budget, genre, etc. The objective of our project is to create a program that will take user inputs for a movie and return an accurate imdb score for it. In order to do this, we will be running different statistical prediction models through Python packages such as scikit-learn and pandas.
+
+
+Currently with the data set, there are a number of missing values. Some columns have hundreds of missing data points. The most missing data points comes from Gross Earnings, which has about 500 missing data points. Other columns have only a few missing data points with the lowest being six missing points. About 16 columns out of the 28 variables have missing values to them.
+
+###Key questions
