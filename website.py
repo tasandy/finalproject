6b3d5a904062ad0a87ddb468cf0db74233152e06
awu1 <awu1@babson.edu>
2016-12-05 18:07:04 -0500

    Update website.py
    with model

--- website.py
+++ website.py
@@ -1,28 +1,33 @@
-from flask import Flask, render_template, request
-from clean_data import get_clean_data
+import numpy as np
+import pandas as pd
 
-app = Flask(__name__)
+from sklearn.linear_model import LogisticRegression
 
+def predict_movie_rating(list_of_user_input):
 
-file_n = 'movie_metadata.csv'
-get_clean_data(file_n)
+    #storing the resopnse variable in 'response' for easier code writing
+    response = moviedata['movie_response']
 
-@app.route('/')
-def hello_world():
-    return render_template('index.html')
+    #create a list of predictors to include in classification model
+    predictor_list = ['title_year', 'director_facebook_likes', 'actor_1_facebook_likes', 'actor_2_facebook_likes', 'actor_3_facebook_likes', 'facenumber_in_poster', 'content_rating', 'budget',
+    'Action','Adventure', 'Animation', 'Biography', 'Comedy', 'Crime', 'Documentary', 'Drama', 'Family', 'Fantasy', 'Film-Noir', 'History', 'Horror', 'Music', 'Musical', 'Mystery', 'News',
+    'Romance', 'Sci-Fi', 'Short', 'Sport', 'Thriller','War', 'Western' ]
+    movie_features = moviedata[predictor_list]
 
-@app.route('/submission', methods=['POST'])
-def login():
-    error = None
-    if request.method == 'POST':
-        name = request.form['name']
-        print name
-    else:
-        error = 'Error: Please fill out all fields'
+    #transform X from pandas dataframe to numpy arrays
+    X = movie_features.as_matrix()
+    #transform y from pandas to 1D list
+    y = list(moviedata['movie_response'].values)
 
-    return render_template('index.html')
+    #--------------- CARRY OUT CLASSIFICATION MODELS ---------------
 
-#def apply_model():
+    #call in the models
+    logreg = LogisticRegression()
 
-if __name__ == '__main__':
-    app.run()
+    # fit the model with our dataset
+    logreg.fit(X, y)
+
+    #make predictions on the testing set
+    log_y_pred = logreg.predict(list_of_user_input)
+
+    print log_y_pred
