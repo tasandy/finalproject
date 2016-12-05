3e8a572086d205a7db80744992ad106befac4202
Wayne Kwon <tkwon2@babson.edu>
2016-12-05 15:34:22 -0500

    classification models

--- classification_models_comments.py
+++ classification_models_comments.py
@@ -5,10 +5,16 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.cross_validation import train_test_split
 from sklearn import metrics
+from sklearn.tree import DecisionTreeClassifier
 
 mydata=pd.read_csv('rough_cleandata.csv')
 
 #--------------- PREPARING FOR CLASSIFICATION MODELS ---------------
+
+#keeping only years after 2000
+
+mydata = mydata.drop(mydata[mydata.title_year < 2000].index)
+print len(mydata)
 
 #converting response variable from string categories to numeric categories
 mydata.loc[mydata["movie_response"]=="Bad", "movie_response"] = 0
@@ -18,8 +24,8 @@ mydata.loc[mydata["movie_response"]=="Good", "movie_response"] = 2
 #storing the resopnse variable in 'response' for easier code writing
 response = mydata['movie_response']
 
-#create a list of predictors to include in the logistic regression
-predictor_list = ['title_year','num_critic_for_reviews', 'duration', 'director_facebook_likes', 'actor_3_facebook_likes', 'actor_1_facebook_likes', 'num_voted_users', 'cast_total_facebook_likes', 'facenumber_in_poster', 'num_user_for_reviews', 'content_rating', 'budget', 'actor_2_facebook_likes',
+#create a list of predictors to include in classification model
+predictor_list = ['title_year', 'director_facebook_likes', 'actor_3_facebook_likes', 'actor_1_facebook_likes', 'facenumber_in_poster', 'content_rating', 'budget', 'actor_2_facebook_likes',
 'Action','Adventure', 'Animation', 'Biography', 'Comedy', 'Crime', 'Documentary', 'Drama', 'Family', 'Fantasy', 'Film-Noir', 'History', 'Horror', 'Music', 'Musical', 'Mystery', 'News', 'Romance', 'Sci-Fi', 'Short','Sport', 'Thriller','War', 'Western' ]
 movie_features = mydata[predictor_list]
 
@@ -31,7 +37,7 @@ y = list(mydata['movie_response'].values)
 
 #--------------- SPLITTING INTO TRAINING & TESTING SETS ---------------
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 2) #to keep splitting constant, set 'random_state=1'
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2)#, random_state = 2) #to keep splitting constant, set 'random_state=1'
 #print X_train.shape, X_test.shape
 #print len(y_train), len(y_test)
 
@@ -40,26 +46,31 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, rando
 
 #call in the models
 logreg = LogisticRegression()
-knn2 = KNeighborsClassifier(n_neighbors=2) # K=2
-knn3 = KNeighborsClassifier(n_neighbors=3) # K=2
+knn2 = KNeighborsClassifier(n_neighbors=15) # K=2
+knn3 = KNeighborsClassifier(n_neighbors=20) # K=3
+dec_tree = DecisionTreeClassifier()
 
 #fit the model with training set
 logreg.fit(X_train, y_train)
 knn2.fit(X_train, y_train)
 knn3.fit(X_train, y_train)
+dec_tree.fit(X_train, y_train)
 
 #make predictions on the testing set
 log_y_pred = logreg.predict(X_test)
 knn2_y_pred = knn2.predict(X_test)
 knn3_y_pred = knn3.predict(X_test)
+dec_tree_y_pred = dec_tree.predict(X_test)
 
 #compare actual response values with predicted values
 log_accuracy = metrics.accuracy_score(y_test, log_y_pred)
 knn2_accuracy = metrics.accuracy_score(y_test, knn2_y_pred)
 knn3_accuracy = metrics.accuracy_score(y_test, knn3_y_pred)
+dec_tree_accuracy = metrics.accuracy_score(y_test, dec_tree_y_pred)
 print "Logitstic Accuracy: ", log_accuracy
 print "KNN (K=2) Accuracy: ", knn2_accuracy
 print "KNN (K=3) Accuracy: ", knn3_accuracy
+print "Decision Tree Accuracy: ", dec_tree_accuracy
 
 #FOR K-NN: find the best value of k by iterating its value
 k_range = range(1,25)
@@ -82,4 +93,4 @@ plt.plot(k_range, scores)
 plt.xlabel('Value of K for KNN')
 plt.ylabel('Testing Accuracy')
 
-#plt.show()
+plt.show()
