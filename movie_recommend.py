300ad0241b8916f4f72c3fe890b91067293d5c30
Wayne Kwon <tkwon2@babson.edu>
2016-12-14 16:36:47 -0500

    movie recommend

--- movie_recommend.py
+++ movie_recommend.py
@@ -7,4 +7,9 @@ def recommend_movie(genre_from_user, data_used_for_recommendation):
     data_used_for_recommendation.sort_values(by='imdb_score')
     #keep only movies of high scores
     data_used_for_recommendation = data_used_for_recommendation.drop(data_used_for_recommendation[data_used_for_recommendation.imdb_score < 7].index)
-    return data_used_for_recommendation['movie_title'][:3]
+
+    movie_for_recommendation = data_used_for_recommendation['movie_title'].map(lambda x: str(x)[:-2])
+    movie_for_recommendation = movie_for_recommendation.tolist()
+
+
+    return movie_for_recommendation[:3]
